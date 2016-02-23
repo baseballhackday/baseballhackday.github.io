acb9c4a8156baeeca5a3cc07cfb80cb46aabb23c
Stephane <stephane@plankdesign.com>
2016-02-23 16:12:21 -0500

    montreal page integration debut

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+
+
+
 $(function(){
   // bind change event to select
   $('.city-select').on('change', function () {
@@ -10,3 +13,13 @@ $(function(){
       return false;
   });
 });
+
+
+//on standby
+// $(function() {
+// 	$("dropdown-button").click(function(){
+// 		var $button, $menu;
+// 		$button = $(this);
+// 		$menu = $buton.siblings(".dropdown-menu");
+// 	})
+// })
