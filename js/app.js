05f5ac01833e2bf08960e6aa83049e838afbec3d
Stephane <stephane@plankdesign.com>
2016-02-25 14:08:44 -0500

    working on index dropdown

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
-$(function(){
-  // bind change event to select
-  $('.city-select').on('change', function () {
-  	  console.log("test");
+// $(function(){
+//   // bind change event to select
+//   $('.city-select').on('change', function () {
+//   	  console.log("test");
 
+//       var url = $(this).val(); // get selected value
+//       if (url) { // require a URL
+//           window.location = url; // redirect
+//       }
+//       return false;
+//   });
+// });
+
+$(document).ready(function() {
+  $(".dropdown-button").click(function() {
+    var $button, $menu;
+    $button = $(this);
+    $menu = $button.siblings(".dropdown-menu");
+    $menu.toggleClass("show-menu");
+    $menu.children("li").click(function() {
+      $menu.removeClass("show-menu");
+      $button.html($(this).html());
+    });
+  });
+
+  $('option').on('click', function () {
       var url = $(this).val(); // get selected value
       if (url) { // require a URL
           window.location = url; // redirect
