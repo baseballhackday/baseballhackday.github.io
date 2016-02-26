7bb2899c6c93b9a2cb7c07cb7c7e1a6aeb7570b4
Daigo Fujiwara <daigo@daigofujiwara.com>
2016-02-26 16:50:07 -0500

    Index pulldown fix for Safari

--- js/app.js
+++ js/app.js
@@ -5,24 +5,10 @@ $(document).ready(function() {
     $button = $(this);
     $menu = $button.siblings(".dropdown-menu");
     $menu.toggleClass("show-menu");
-    $menu.children("li").click(function() {
-      $menu.removeClass("show-menu");
-      $button.html($(this).html());
-    });
-  });
-
-  $('option').on('click', function () {
-      var url = $(this).val(); // get selected value
-      if (url) { // require a URL
-          window.location = url; // redirect
-      }
-      return false;
   });
 
     // bind change event to select
   $('.city-select').on('change', function () {
-  	  console.log("test");
-
       var url = $(this).val(); // get selected value
       if (url) { // require a URL
           window.location = url; // redirect
