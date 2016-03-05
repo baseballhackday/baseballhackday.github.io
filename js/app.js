59e1b574df12814d1a9a7c7b784fe06bf83130d8
Wil Chandler <wilchandler2@gmail.com>
2016-03-05 15:43:28 -0600

    Fixes index dropdown overflow

--- js/app.js
+++ js/app.js
@@ -7,4 +7,26 @@ $(document).ready(function() {
     $menu.toggleClass("show-menu");
   });
 
+  if ($('.hero').length) {
+    $(window).resize(assignIndexHeroHeight);
+    $(window).trigger('resize');
+  }
+
 });
+
+function assignIndexHeroHeight() {
+  var $body = $('body');
+  var $window = $(window);
+  var $hero = $('.hero');
+  var $cities = $hero.find('.dropdown-select a');
+  var $indexDropdownButton = $hero.find('.dropdown-button');
+  var buttonHeight = $indexDropdownButton.height();
+  var minHeight = $body.height() + (buttonHeight * ($cities.length + 1));
+  var windowHeight = $window.height();
+
+  if (windowHeight > minHeight) {
+    $hero.css('height', '100vh');
+  } else {
+    $hero.height(minHeight);
+  }
+}
