bd602b9fb62c442a499d530d63d91b0ca2bbd060
Wil Chandler <wilchandler2@gmail.com>
2016-03-05 16:02:23 -0600

    Fixes size issue caused by TypeKit on smaller screens

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
     $menu.toggleClass("show-menu");
   });
 
+  // If home page, set up function for 
+  // document/hero sizing.
   if ($('.hero').length) {
     $(window).resize(assignIndexHeroHeight);
     $(window).trigger('resize');
@@ -20,6 +22,12 @@ function assignIndexHeroHeight() {
   var $hero = $('.hero');
   var $cities = $hero.find('.dropdown-select a');
   var $indexDropdownButton = $hero.find('.dropdown-button');
+
+  // If TypeKit hasn't loaded, button could be wrapping on
+  // smaller screens and therefore too large.
+  var originalWhiteSpace = $indexDropdownButton.css('white-space');
+  $indexDropdownButton.css('white-space', 'nowrap');
+
   var buttonHeight = $indexDropdownButton.height();
   var minHeight = $body.height() + (buttonHeight * ($cities.length + 1));
   var windowHeight = $window.height();
@@ -29,4 +37,7 @@ function assignIndexHeroHeight() {
   } else {
     $hero.height(minHeight);
   }
+
+  // Reset original CSS
+  $indexDropdownButton.css('white-space', originalWhiteSpace);
 }
