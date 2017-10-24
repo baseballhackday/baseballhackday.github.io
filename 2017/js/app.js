4007a61592c970aed23e3ec4fbc2a84abf2fb649
Daigo Fujiwara <daigo@daigofujiwara.com>
2017-10-24 14:54:59 -0400

    archive 2017 contents

---
+++ 2017/js/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  //index dropdown
+  $(".dropdown-button").click(function() {
+    var $button, $menu;
+    $button = $(this);
+    $menu = $button.siblings(".dropdown-menu");
+    $menu.toggleClass("show-menu");
+  });
+
+  // If home page, set up function for 
+  // document/hero sizing.
+  if ($('.hero').length) {
+    $(window).resize(assignIndexHeroHeight);
+    $(window).trigger('resize');
+  }
+
+});
+
+function assignIndexHeroHeight() {
+  var $window = $(window);
+  var $hero = $('.hero');
+  var $heroContent = $('.hero-content');
+  var $cities = $heroContent.find('.dropdown-select a');
+  var $indexDropdownButton = $heroContent.find('.dropdown-button');
+
+  // If TypeKit hasn't loaded, button could be wrapping on
+  // smaller screens and therefore too large.
+  var originalWhiteSpace = $indexDropdownButton.css('white-space');
+  $indexDropdownButton.css('white-space', 'nowrap');
+
+  var buttonHeight = $indexDropdownButton.height();
+  var minHeight = $heroContent.height() + (buttonHeight * ($cities.length + 1));
+  var windowHeight = $window.height();
+
+  if (windowHeight > minHeight) {
+    $hero.css('height', '100vh');
+  } else {
+    $hero.height(minHeight);
+  }
+
+  // Reset original CSS
+  $indexDropdownButton.css('white-space', originalWhiteSpace);
+}
